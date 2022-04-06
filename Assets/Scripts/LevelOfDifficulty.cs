90c5c6ac0ea268d04682c0e6b2961c3f4560a752
Negar <neg_r.saeidi95@yahoo.com>
2022-04-06 14:35:51 -0500

    Scene Transition added

--- Assets/Scripts/LevelOfDifficulty.cs
+++ Assets/Scripts/LevelOfDifficulty.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class LevelOfDifficulty : MonoBehaviour
 {
-   public void OnEasy()
+    public TextMeshProUGUI colorHint;
+    public TextMeshProUGUI placeHint;
+    public void OnEasy()
     {
         GridGeneration.gridSize = 10;
         GridGeneration.regenerateTiles = true;
+        colorHint.text =  "";
+        placeHint.text =  "";
     }
     public void OnMedium()
     {
         GridGeneration.gridSize = 5;
         GridGeneration.regenerateTiles = true;
+        colorHint.text = "";
+        placeHint.text = "";
     }
     public void OnHard()
     {
         GridGeneration.gridSize =3;
         GridGeneration.regenerateTiles = true;
+        colorHint.text = "";
+        placeHint.text = "";
     }
 }
