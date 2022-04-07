e2ec3e268f9a4bafb2201ad729a93c0ecdd5a2a3
Negar <neg_r.saeidi95@yahoo.com>
2022-04-06 21:23:28 -0500

    TIme up condition added

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,6 +6,7 @@ public class Timer : MonoBehaviour
 {
     public GameObject timerText;
     public static float time;
+    public GameObject gameOverPanel;
     private void Start()
     {
         time = 120;
@@ -16,6 +17,8 @@ public class Timer : MonoBehaviour
         {
             time -= Time.deltaTime;
         }
+        else
+            gameOverPanel.SetActive(true);
         timerText.GetComponent<TextMeshProUGUI>().text = Mathf.FloorToInt(time / 60) + " : " + Mathf.FloorToInt(time % 60);
     }
 }
